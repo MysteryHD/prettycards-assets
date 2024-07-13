b1c9c15851b2d7e0a24ba3fce830372e632dd152
elytrafae <48065803+elytrafae@users.noreply.github.com>
2024-07-13 11:15:01 +0300

    File upload

---
+++ workflow_script/on_push.js
@@ -0,0 +1,93 @@
+
+var fs = require('fs');
+
+/*
+Stuff that this should do:
+- Log and transform theme songs
+- Log HD skins
+
+*/
+
+const HD_SKIN_IMAGE_PATH = "../img/HDCardSkins";
+const HD_SKIN_JSON_PATH = "../json/hdCardSkins.json";
+const INSET_THEME_SONGS_PATH = "../INSERT_THEME_SONGS_HERE";
+const CARDS_AUDIO_PATH = "../audio/cards";
+const CARDS_AUDIO_JSON_PATH = "../json/baseThemeSongData.json";
+const CARDS_AUDIO_DATA_THAT_SHOULD_CARRY_OVER = ["_Disabled_Themes"];
+
+function MapHDSkins() {
+	var images = fs.readdirSync(HD_SKIN_IMAGE_PATH);
+	var data = images.map(RemoveFileExtension);
+	
+	fs.writeFileSync(HD_SKIN_JSON_PATH, JSON.stringify(data, null, 4));
+}
+
+function TransformThemeSongs() {
+	for (var filename in fs.readdirSync(INSET_THEME_SONGS_PATH)) {
+		if (!filename.endsWith(".ogg")) {
+			continue;
+		}
+		var myFormat = GetMyFileNameAndFolderFromMysterysFormat(filename);
+		fs.writeFileSync(`${CARDS_AUDIO_PATH}/${myFormat.folder}/intro_${myFormat.index}.ogg`, fs.readFileSync(`${INSET_THEME_SONGS_PATH}/${filename}`));
+	}
+}
+
+function MapThemeSongs() {
+	var data = {};
+	var dirs = getDirectories(CARDS_AUDIO_PATH);
+	dirs.forEach(dir => {
+		console.log(`   Started On: ${dir}`);
+		
+		var dirContents = fs.readdirSync(`${CARDS_AUDIO_PATH}/${dir}`);
+		//var maxNum = dirContents.map(GetMyFileNameAndFolderFromMysterysFormat).sort((a, b) => b.index - a.index)[0]; // This is a very disgusting hack, but it *should* work . . .
+		var maxNum = dirContents.length; // Nevermind, I am stoopid
+		data[dir.replace(/_/gm, " ")] = maxNum;
+	});
+	var oldData = JSON.parse(fs.readFileSync(CARDS_AUDIO_JSON_PATH).toString());
+	CARDS_AUDIO_DATA_THAT_SHOULD_CARRY_OVER.forEach(prop => {data[prop] = oldData[prop];})
+	fs.writeFileSync(CARDS_AUDIO_JSON_PATH, JSON.stringify(data, null, 4));
+}
+
+function RemoveFileExtension(/** @type {string} */ fileName) {
+	var arr = fileName.split(".");
+	arr.splice(arr.length-1, 1);
+	return arr.join(".");
+}
+
+function GetMyFileNameAndFolderFromMysterysFormat(/** @type {string} */ name) {
+	var name2 = RemoveFileExtension(name);
+	if (name2.length <= 0) {
+		throw new Error(`GetMyFileNameAndFolderFromMysterysFormat received an invalid filename (empty) "${name}" ("${name2}" after trimming the file extension)`);
+	}
+	var digit = name2.charAt(name2.length-1);
+	var number = 0;
+	var p = 1;
+	while (name2.length > 0 && !isNaN(parseInt(digit))) {
+		number += p*parseInt(digit);
+		p *= 10;
+		name2.substring(0,name2.length-1);
+		digit = name2.charAt(name2.length-1);
+	}
+	if (number == 0) {
+		number = 1;
+	}
+	if (name2.length <= 0) {
+		throw new Error(`GetMyFileNameAndFolderFromMysterysFormat received an invalid filename (numbers only) "${name}" ("${name2}" after trimming the file extension and number)`);
+	}
+	return {folder: name2, index: number};
+}
+
+// This handly little thing is from StackOverflow, but nobody needs to know that
+const getDirectories = source =>
+	fs.readdirSync(source, { withFileTypes: true })
+	  .filter(dirent => dirent.isDirectory())
+	  .map(dirent => dirent.name)
+
+
+console.log("Starting . . .");
+MapHDSkins();
+console.log("HD Skins Mapped . . .");
+TransformThemeSongs();
+console.log("Theme songs transformed . . .");
+MapThemeSongs();
+console.log("Theme Songs Mapped! Done!");
